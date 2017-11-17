5fef7e6104f9b8c7a52b2552bb058368329e90b4
zonghai.szh <zonghai.szh@alibaba-inc.com>
2017-11-17 15:53:44 +0800

    add maven-compiler-plugin

--- src/main/com/caucho/hessian/test/HelloMain.java
+++ src/main/com/caucho/hessian/test/HelloMain.java
@@ -2,7 +2,7 @@ package com.caucho.hessian.test;
 
 public class HelloMain {
     public static void main(String[] args) {
-        System.out.println(int.class.getName());
-
+        double pre = 125.6f;
+        System.out.println((int)pre);
     }
 }
